918d4a515035b75fbe8699a4e6fbd2f79494b14c
DmAglr <daniel.maguilar2002@gmail.com>
2026-06-10 10:04:29 -0300

    atualizando para fazer ajuste

--- .bat
+++ .bat
@@ -1,3 +1,3 @@
 cd "C:\Automacoes\Multas\Multas_33-Comprovante-Postagem-Correios"
-node "src/main.js" >> "S:\Automacoes\Multas\33 - Comprovante postagem Correios\Retorno\log.txt"
+node "src/main.js"
 pause
